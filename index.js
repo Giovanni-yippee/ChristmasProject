129f55990cbd96e8fd7200b85b958276b19b79c9
Akerke <muiskar.demon@gmail.com>
2024-12-26 19:37:21 +0500

    ҺҺҺҺҺҺҺҺҺҺҺ# Please enter the commit message for your changes. Lines starting

---
+++ index.js
@@ -0,0 +1,27 @@
+function startCountdown() {
+    const countdownDate = new Date("Dec 29, 2024 23:59:59").getTime();
+
+    function updateCountdown() {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+
+        if (distance < 0) {
+            clearInterval(interval);
+            document.querySelector(".countdown .timer").innerHTML = "EXPIRED";
+        }
+    }
+
+    const interval = setInterval(updateCountdown, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", startCountdown);
